dde99c57ea472569a2f75650e4fe8525f6fcbeb6
Yiming <sunstat@stanford.edu>
2018-03-11 14:55:32 -0400

    add util

---
+++ util.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def randomMatrixGenerator(m, n, std, type='g'):
+    '''
+    :param m: # of rows in the matrix
+    :param n: # of columns in the matrix
+    :param std: standard deviation
+    :param type: 'g' for gauss, 'u' for uniform, 'h'
+    :return: the generated matrix
+    '''
+
+
+
+

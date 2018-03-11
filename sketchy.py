074ff68193bea992260ecd86b0540c469bc7f9b4
Yiming <sunstat@stanford.edu>
2018-03-11 15:35:29 -0400

    add

---
+++ sketchy.py
@@ -0,0 +1,13 @@
+class Skechy():
+    def __init__(self, X, rand_seed, k, s):
+        '''
+        :param X:
+        :param rand_seed:
+        :param k:
+        :param s:
+        '''
+        self.X = X
+
+
+    def get_skechy(self):
+
